8134c2dde54cf2fa2f4751d1e95aee884a38fa2f
rodgers <rodgersny99@gmail.com>
2019-04-24 10:20:35 +0300

    toggling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailsComponent
   ],
   imports: [
     BrowserModule,
