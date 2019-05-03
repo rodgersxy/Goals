66b796f522ee8266bb8067f4bbf5db0fd36b2ae0
rodgers <rodgersny99@gmail.com>
2019-05-03 12:34:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import{FormsModule} from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { AlertsService } from './alert-service/alerts.service'
+import { HttpClientModule } from '@angular/common/http'
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RoutingModule } from './routing/routing.module'
+
+//Defining routes
+const routes:Routes=[
+  {path:"goals",component:GoalComponent},
+  {path:"about",component:AboutComponent},
+  {path:"",redirectTo:"/goals",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,13 +33,22 @@ import { DateCountPipe } from './date-count.pipe';
     GoalComponent,
     GoalDetailsComponent,
     StrikethroughDirective,
-    DateCountPipe
+    DateCountPipe,
+    GoalFormComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RoutingModule,
+    HttpClientModule,
+    NgProgressModule,// normal progress bar
+    NgProgressHttpModule, // progress bar to load http request
+    RouterModule.forRoot(routes)
+    
   ],
-  providers: [],
+  providers: [AlertsService], // Add service to the providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
